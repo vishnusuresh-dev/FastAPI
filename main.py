297b6200ec5d73a16ad34b22d59aa19c06865baf
Vishnu <myworkspacexx@gmail.com>
2026-02-06 11:32:33 +0530

    fast-api workout 1

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from scalar_fastapi import get_scalar_api_reference
+app = FastAPI()
+
+@app.get("/shipment")
+def get_shipment():
+  return {
+    "status":"In transit"
+  }
+  
+@app.get("/scalar", include_in_schema=False)
+def get_scalar_doc():
+  return get_scalar_api_reference(
+    openapi_url=app.openapi_url,
+    title = "Scalar API docs"
+  )
